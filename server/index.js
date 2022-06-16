a87c04c439a9de31fa3054d84057d05aa7275d46
Ing. Benito Enríquez Mora <enriquez.ico@gmail.com>
2022-06-16 12:27:30 -0500

    feat(add-server): config server express and api graphql.
    Config test typeDefs and resolvers.

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+const { typeDefs } = require("./graphql/test/typeDefs");
+const { resolvers } = require("./graphql/test/resolver");
+
+const app = express();
+
+// method init server mode async
+const start = async () => {
+    //  create instance of ApolloServer
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers
+    });
+    //  init Apollo Server
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    //  config others routes 404
+    app.use("*", (req, res) => { res.status(404).send("Not found.") })
+
+    app.listen(3000, () => {
+        console.log("Server on port ", 3000);
+    });
+};
+
+start();
+
+module.exports = app;
